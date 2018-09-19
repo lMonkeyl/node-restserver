c2ddad23cd3bd7ad2f3c907fd1f5d94c126620e1
Monkey <hmespinosa99@gmail.com>
2018-09-19 11:39:34 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds163402.mlab.com:63402/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
