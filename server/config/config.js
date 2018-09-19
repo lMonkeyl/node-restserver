b415ef32847509b344e088b8f5d1875f0ccdf137
Monkey <hmespinosa99@gmail.com>
2018-09-19 11:11:42 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,16 @@
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:abc123@ds163402.mlab.com:63402/cafe';
+}
+
+process.env.URLDB = urlDB;
